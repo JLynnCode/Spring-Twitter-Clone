72967dcd6c4239354c7ff745255b7a29d8484cf2
gazgiz <jlynnmail@gmail.com>
2022-10-31 13:31:41 -0500

    Added basic files for TweetService and TweetServiceImpl

---
+++ src/main/java/com/cooksys/twitterclone/services/impl/TweetServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cooksys.twitterclone.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class TweetServiceImpl {
+
+}
