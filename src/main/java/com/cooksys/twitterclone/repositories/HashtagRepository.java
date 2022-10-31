1a2b506de8ef8a5e27ed127e8007c149ca67efb1
gazgiz <jlynnmail@gmail.com>
2022-10-31 16:21:01 -0500

    Added basic files for User/Tweet/Hashtag Repositories

---
+++ src/main/java/com/cooksys/twitterclone/repositories/HashtagRepository.java
@@ -0,0 +1,7 @@
+package com.cooksys.twitterclone.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+
+}
